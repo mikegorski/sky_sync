2ca6e5d004ddd4094e4f24b110182c487733bea2
mikegorski <mike.gorski96@gmail.com>
2024-04-07 13:29:44 +0200

    [FEAT] implemented Dashboard object along with signal for its creation

--- sky_sync/users/models.py
+++ sky_sync/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from open_weather_api.models import Geolocation
 
 
 class User(AbstractUser):
@@ -7,3 +8,11 @@ class User(AbstractUser):
     last_login = models.DateTimeField(null=True, default=None)
     is_active = models.BooleanField(default=True)
     ip = models.GenericIPAddressField(null=True, default=None)
+
+
+class Dashboard(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    geolocations = models.ManyToManyField(to=Geolocation, blank=True)
+
+    def __str__(self):
+        return f"{self.user}'s Dashboard"
