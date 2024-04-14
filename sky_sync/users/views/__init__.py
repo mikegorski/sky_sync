c8e1e55dbcfdf6ab8360a70dca481d74fd1e0c48
mikegorski <mike.gorski96@gmail.com>
2024-04-14 19:44:00 +0200

    [FEAT] refactored HTMLs for better display, implemented initial dashboard version and refactored user views

---
+++ sky_sync/users/views/__init__.py
@@ -0,0 +1,4 @@
+from .auth import ActivationView, RegisterView, CustomLoginView, AfterRegistrationView
+from .regular import AboutView, LandingView, DashboardView
+
+__all__ = [AboutView, ActivationView, AfterRegistrationView, CustomLoginView, DashboardView, LandingView, RegisterView]
